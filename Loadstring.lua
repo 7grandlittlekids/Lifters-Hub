27a030d65110f6c8335c1ce46b1193cd3844e535
Sabi <128085045+7grandlittlekids@users.noreply.github.com>
2025-03-29 16:58:39 -0600

    Update Loadstring.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,3 +1,4 @@
 
-loadstring(game:GetHttp("https://raw.githubusercontent.com/7grandlittlekids/Lifters-Hub/refs/heads/main/Protected_6564116674829728.lua.txt"))()
-loadstring(game:GetHttp("https://raw.githubusercontent.com/7grandlittlekids/Lifters-Hub/refs/heads/main/Protected_7199643113714550.lua.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/7grandlittlekids/Lifters-Hub/refs/heads/main/Protected_6564116674829728.lua.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/7grandlittlekids/Lifters-Hub/refs/heads/main/Protected_7199643113714550.lua.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/7grandlittlekids/Lifters-Hub/refs/heads/main/Protected_9046821912765843.lua.txt"))()
