669e60dc30120b0082dbfa0494877c68c7f5ab77
Sabi <128085045+7grandlittlekids@users.noreply.github.com>
2025-03-29 16:57:32 -0600

    Update Loadstring.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,2 +1,3 @@
 
 loadstring(game:GetHttp("https://raw.githubusercontent.com/7grandlittlekids/Lifters-Hub/refs/heads/main/Protected_6564116674829728.lua.txt"))()
+loadstring(game:GetHttp("https://raw.githubusercontent.com/7grandlittlekids/Lifters-Hub/refs/heads/main/Protected_7199643113714550.lua.txt"))()
