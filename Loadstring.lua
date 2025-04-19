9d20f02b2b7b1fa2081975033c6da7685407930b
Sabi <128085045+7grandlittlekids@users.noreply.github.com>
2025-04-19 07:54:00 -0600

    Update Loadstring.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,5 +1,5 @@
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/7grandlittlekids/Lifters-Hub/refs/heads/main/Protected_6564116674829728.lua.txt"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/7grandlittlekids/Lifters-Hub/refs/heads/main/Protected_7199643113714550.lua.txt"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/7grandlittlekids/Lifters-Hub/refs/heads/main/Protected_9046821912765843.lua.txt"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/7grandlittlekids/Lifters-Hub/refs/heads/main/Protected_9123438316333474.lua.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/7grandlittlekids/Lifters-Hub/refs/heads/main/Moonsec%20Obfuscated%20Code/Protected_6564116674829728.lua.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/7grandlittlekids/Lifters-Hub/refs/heads/main/Moonsec%20Obfuscated%20Code/Protected_7199643113714550.lua.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/7grandlittlekids/Lifters-Hub/refs/heads/main/Moonsec%20Obfuscated%20Code/Protected_9046821912765843.lua.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/7grandlittlekids/Lifters-Hub/refs/heads/main/Moonsec%20Obfuscated%20Code/Protected_9123438316333474.lua.txt"))()
