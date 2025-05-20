f2a633fba1dfaa615084ca781f212111b15313f3
Sabi <128085045+7grandlittlekids@users.noreply.github.com>
2025-05-19 18:25:55 -0600

    Update Loadstring.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -4,3 +4,4 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/7grandlittlekids/Lift
 loadstring(game:HttpGet("https://raw.githubusercontent.com/7grandlittlekids/Lifters-Hub/refs/heads/main/Moonsec%20Obfuscated%20Code/Protected_9046821912765843.lua.txt"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/7grandlittlekids/Lifters-Hub/refs/heads/main/Moonsec%20Obfuscated%20Code/Protected_9123438316333474.lua.txt"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/7grandlittlekids/Lifters-Hub/refs/heads/main/Moonsec%20Obfuscated%20Code/Protected_7408140144798768.lua.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/7grandlittlekids/Lifters-Hub/refs/heads/main/Moonsec%20Obfuscated%20Code/Protected_2698410696697554.lua.txt"))()
