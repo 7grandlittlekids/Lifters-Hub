49a479347221b7210fa24ebb02384b21d52d3c16
Sabi <128085045+7grandlittlekids@users.noreply.github.com>
2025-05-23 16:29:41 -0600

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -1,1 +1,11 @@
+-- If ur using this script for Old Toilet TD, Include this lil script below
+--[[
+getgenv().customAbbreviations = {
+    ["Hyper Upgraded Titan Speakerman"] = "HUTS",
+    ["Green Laser Cameraman"] = "GLC",
+    ["Santa TV Man"] = "STVM",
+    ["Scientist TV Man"] = "STM",
+    ["DJ TV Man"] = "DTM",
+}
+--]]
 loadstring(game:HttpGet("https://raw.githubusercontent.com/7grandlittlekids/Lifters-Hub/refs/heads/main/Loadstring.lua"))()
